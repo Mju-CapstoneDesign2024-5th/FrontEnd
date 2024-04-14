39689184377edfe05a2c544ece7456c87af6f40d
Jake-Kim38 <kjm25034841@gmail.com>
2024-04-15 00:51:37 +0900

    feat: 검색바 구현

---
+++ src/components/Searchbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const Searchbar = () => {
+    return (
+        <SearchbarBox>
+            <FontAwesomeIcon icon={faMagnifyingGlass} color="#79D6D2" size="lg" />
+            <InputBox type="text" placeholder="Search"/>
+        </SearchbarBox>
+    )
+}
+
+const SearchbarBox = styled.div`
+    width: 442px;
+    height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.GRAY};
+    border: thin solid white;
+    border-radius: 5px;
+`
+
+const InputBox = styled.input`
+    font-size: 20px;
+    width: 85%;
+    text-align: center;
+    background-color: ${({ theme }) => theme.colors.GRAY};
+    border: none;
+    color: white;
+    outline: none;
+`
+
+export default Searchbar;
