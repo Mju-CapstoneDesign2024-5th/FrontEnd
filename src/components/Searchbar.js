8762053ab38fb050010a9b90d80d353676bbb6f7
Jake-Kim38 <kjm25034841@gmail.com>
2024-04-16 23:34:00 +0900

    feat: 프로필/설정 페이지 구현

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -21,6 +21,7 @@ const SearchbarBox = styled.div`
     background-color: ${({ theme }) => theme.colors.GRAY};
     border: thin solid white;
     border-radius: 5px;
+    margin-left: 20%;
 `
 
 const InputBox = styled.input`
